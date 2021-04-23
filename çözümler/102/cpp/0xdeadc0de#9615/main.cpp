b2e0de84a17587f6e645e5bf9aa881fca0ae0893
0xdeadc0de <14943826+0xdeadc0de@users.noreply.github.com>
2021-04-23 17:53:34 +0300

    hello window ok sfml

--- çözümler/102/cpp/0xdeadc0de#9615/main.cpp
+++ çözümler/102/cpp/0xdeadc0de#9615/main.cpp
@@ -1,7 +1,24 @@
-#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    sf::sleep(sf::seconds(1.f));
+    sf::ContextSettings s;
+    s.depthBits = 24;
+    s.stencilBits = 8;
+    s.antialiasingLevel = 2;
+    s.majorVersion = 3;
+    s.minorVersion = 2;
+    s.attributeFlags = sf::ContextSettings::Core;
+
+    sf::Window window(sf::VideoMode(800,600), "SFML hello window", sf::Style::Close, s);
+
+    for (;;) {
+        sf::Event e;
+        while (window.pollEvent(e)) {
+            if (e.type == sf::Event::Closed) {
+                return 0;
+            }
+        }
+    }
     return 0;
 }
